bf6426797dc608ff11bf65f2413fd9e9d82f1324
TulipJani <tkjani20@gmail.com>
2024-03-02 15:27:28 +0530

    final commit

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function locoTrigger() {
   gsap.registerPlugin(ScrollTrigger);
-
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
+    smoothMobile: true,
+    smartphone: {
+      smooth: true,
+    },
     smooth: true,
   });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
-
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
   ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length
